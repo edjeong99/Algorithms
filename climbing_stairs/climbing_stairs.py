dbf7ea3f0b57be619c4ab7fc40272c48c2dff4ab
Edward J <edjeong99@gmail.com>
2019-02-20 16:24:09 -0500

    add comment

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -5,10 +5,10 @@ import sys
 
 
 def climbing_stairs(n, cache=None):
-    if cache is None:
+    if cache is None:  # if there is no cache, initialize cache as array of 0's
         cache = [0 for i in range(n+1)]
 
-    if n == 0:  # if input is 0, return 0.  this is true only when input is 0
+    if n == 0:  # if input is 0, return 1.  this is true only when input is 0
         return 1
     elif n == 1:
         return 1
@@ -19,12 +19,12 @@ def climbing_stairs(n, cache=None):
     elif n == 3:  # if 3 steps left, there are 4 ways to climb
         return 4
 
-    if cache[n] > 0:
+    if cache[n] > 0:  # if there is value in cache index of n, use it
         return cache[n]
     else:
         val = climbing_stairs(
             n - 1, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 3, cache)
-        cache[n] = val
+        cache[n] = val  # assgin value to cache index of n
         return val
 
 
