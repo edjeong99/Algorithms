aad60f50bd6a2e39018b1bfb2ee2319addfc4edb
Edward J <edjeong99@gmail.com>
2019-02-21 18:01:13 -0500

    tried to use cache in change but failed

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,18 +3,31 @@
 import sys
 
 
-def making_change(amount, denominations, denom=None):
+def making_change(amount, denominations, coin=None, cache=None):
 
-    if denom is None:
-        denom = len(denominations)-1
+    # initialize 2 variables
+    if coin is None:  # coin is index for denominations
+        coin = len(denominations)-1
 
-    # base - if amount is reached return true
-    if amount == 0:
-        return 1
-    elif amount < 0 or denom < 0:
+# cache for quicker run time
+    if cache is None:
+        cache = {i: 0 for i in range(amount + 1)}
+
+   # base case for recursive - if amount is reached return true
+
+    if amount < 0 or coin < 0:
         return 0
+    elif cache[amount] > 0:
+        print(f'RETURNING  amount = {amount}  cache = {cache[amount]}')
+        return cache[amount]
+    elif amount == 0:
+        return 1
 
-    return making_change(amount, denominations, denom-1) + making_change(amount - denominations[denom], denominations, denom)
+    cache[amount] = making_change(amount, denominations, coin-1, cache) + \
+        making_change(
+            amount - denominations[coin], denominations, coin,  cache)
+    print(f'ASSIGNING  amount = {amount}  cache = {cache[amount]}')
+    return cache[amount]
 
 
 if __name__ == "__main__":
